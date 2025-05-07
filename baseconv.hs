792a885396916b04ddf4f224b167977391b1f6c4
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-05-07 05:37:45 -0400

    feat: Implemented the baseconv function

--- baseconv.hs
+++ baseconv.hs
@@ -1,3 +1,19 @@
+base2dec :: Int -> Int -> Int
+base2dec _ 0 = 0
+base2dec a num = num `mod` 10 +  a * (base2dec a (num `div` 10))
+
+dec2base :: Int -> Int -> Int
+dec2base _ 0 = 0
+dec2base a num = num `mod` a + 10 * dec2base a (num `div` a)
+
 baseconv :: Int -> Int -> Int -> Int
-baseconv _ _ 0 = 0
-baseconv a b num = a `mod` 2 + 10 * dec2bin (a `div` 2)
+baseconv a b num = dec2base b (base2dec a num)
+
+
+
+
+main :: IO()
+main = do
+    print(baseconv 2 10 10001)
+    print(baseconv 10 2 17)
+    print(baseconv 2 8 11110010111)
